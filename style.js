18f2eceb5becb9348673d991ef92dd0cc30bb692
Akuzawhite <160669373+Akuzawhite@users.noreply.github.com>
2025-12-01 20:07:28 +0100

    Add files via upload

--- style.js
+++ style.js
@@ -4,3 +4,9 @@ form.addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Réservation envoyée ! Nous vous contacterons rapidement.");
 });
+const burger = document.querySelector('.burger');
+const links = document.querySelector('.links');
+
+burger.addEventListener('click', () => {
+  links.classList.toggle('active');
+});
